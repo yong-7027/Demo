c766743210269a8a12ec2325ade3f62869d9a775
yong-7027 <cytan-pm22@student.tarc.edu.my>
2024-11-21 12:19:27 +0800

    Add project files.

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly DB db;
+	public HomeController(DB db)
+    {
+        this.db = db;
+	}
+
+    // GET: Home/Index
+    public IActionResult Index()
+    {
+        var m = db.Students.Include(s => s.Program);
+
+        return View(m);
+    }
+
+    // GET: Home/Detail
+    public IActionResult Detail(string? id)
+    {
+        var m = db.Students.Include(s => s.Program).FirstOrDefault(s => s.Id == id);
+        if (m == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View(m);
+    }
+}
---
+++ Demo/Models/DB.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models;
+
+public class DB : DbContext
+{
+    public DB(DbContextOptions<DB> options) : base(options) { }
+
+    // DbSet
+    public DbSet<Program> Programs { get; set; }
+	public DbSet<Student> Students { get; set; }
+}
+
+// Entity Classes -------------------------------------------------------------
+
+#nullable disable warnings
+
+public class Program
+{
+    // Column
+    [Key, MaxLength(3)]
+    public string Id { get; set; }
+    [MaxLength(100)]
+	public string Name { get; set; }
+
+
+    // Navigation
+    public List<Student> Students { get; set; } = [];
+}
+
+public class Student
+{
+	// Column
+	[Key, MaxLength(10)]
+	public string Id { get; set; }
+	[MaxLength(100)]
+	public string Name { get; set; }
+	[MaxLength(1)]
+	public string Gender { get; set; }
+	 
+	// FK
+	public string ProgramId { get; set; }
+
+	// Navigation
+	public Program Program { get; set; }
+	
+}
